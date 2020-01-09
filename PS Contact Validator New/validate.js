86f344049775ee8d199ea19cf6f1d3949e8bc3d0
chicosonico <30462314+chicosonico@users.noreply.github.com>
2020-01-09 10:38:32 +0100

    Add files via upload
    Funcionando y corregido estéticamente.

--- PS Contact Validator New/validate.js
+++ PS Contact Validator New/validate.js
@@ -1,6 +1,7 @@
 
 
-            function validar_email(email) {
+            
+             function validar_email(email) {
                 var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
                 return regex.test(email) ? true : false;
             }
@@ -8,22 +9,21 @@
                 var error = 0;
                 var n = document.forms["myForm"]["fname"].value;
                 var e = document.forms["myForm"]["email"].value;
+
                 if (n == "") {
                     var f = document.getElementById("firstname_error").innerHTML = "Name must be filled out";
-                    error = 1;
-                   
-                } else if
-                    (e == "") {
-                        var f = document.getElementById("firstname_error").innerHTML = "";
-                    var em = document.getElementById("email_error").innerHTML = "email must be filled out";
+                    error = 1;  
+                } 
+
+                if (e == "") {
+                     var em = document.getElementById("email_error").innerHTML = "email must be filled out";
                     error ++;
                 } else if (validar_email(e)== false) {
-                    var em = document.getElementById("email_error").innerHTML = "";
                     var wf = document.getElementById("wrongf_error").innerHTML = "Wrong format email.";
                     error++;
-                } else {
-                    var wf = document.getElementById("wrongf_error").innerHTML = "";
-                }if (error == 0){
+                }
+
+                if (error == 0){
                     return true;
 
                 }else {
